b2d8b788bea61b9cff8e3446994846b8338dc901
Nurbaki Vural <nurbakivural@gmail.com>
2023-05-31 19:46:21 +0300

    Init

---
+++ src/components/SignOutButton.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import React, { useState } from "react"
+import Button from "./ui/Button"
+import { signIn, signOut } from "next-auth/react"
+
+type Props = {}
+
+const SignOutButton = (props: Props) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const signUserOut = async () => {
+    setIsLoading(true)
+    try {
+      await signOut()
+    } catch (error) {
+      toast({
+        title: "Error signing out",
+        message: "Please try again later",
+        type: "error",
+      })
+    }
+  }
+
+  return (
+    <Button onClick={signUserOut} isLoading={isLoading}>
+      Sign Out
+    </Button>
+  )
+}
+
+export default SignOutButton
